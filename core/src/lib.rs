5e8b8b8aad3b18be0fb9e9abb5321d7b79c4a25e
Michl Steglich <mrgreaka@gmail.com>
2019-05-17 08:52:16 +0200

    fmt

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -3,9 +3,13 @@ pub use rest_client_codegen::rest;
 use std::*;
 
 pub trait ClientMethods {
-    fn get(parameters: impl IntoIterator<Item = impl fmt::Display>) -> Result<Box<Self>, Box<error::Error>>;
+    fn get(
+        parameters: impl IntoIterator<Item = impl fmt::Display>,
+    ) -> Result<Box<Self>, Box<error::Error>>;
 }
 
 pub trait ClientVecMethods {
-    fn gets(parameters: impl IntoIterator<Item = impl fmt::Display>) -> Result<Vec<Box<Self>>, Box<error::Error>>;
+    fn gets(
+        parameters: impl IntoIterator<Item = impl fmt::Display>,
+    ) -> Result<Vec<Box<Self>>, Box<error::Error>>;
 }
